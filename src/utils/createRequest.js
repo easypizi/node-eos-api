8c88022643fc4ed5ca087151b0c5232b78bbd96f
user <user@n431.local>
2018-11-20 19:44:02 +0300

    release version 0.1.3

--- src/utils/createRequest.js
+++ src/utils/createRequest.js
@@ -12,7 +12,8 @@ async function createRequest(host, params) {
   const url = `${host}?${query}`
   const resp = await request(url)
   const result = JSON.parse(resp)
-  if (result.errno !== 0 || result.errmsg !== 'Success') {
+
+  if (result.errno !== 0) {
     const message = result.errmsg
     throw new Error(message)
   }
